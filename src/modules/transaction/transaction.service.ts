b69b0337f3f057c5241fa1c893fb89e3a3d01210
Nourankh20 <nourankh201617@gmail.com>
2021-11-23 20:21:51 +0200

    added account files
    account.module.ts
    account.service.ts
    account.controller.ts

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -3,4 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TransactionService {
   // TODO: Define your Transaction Service Logic
+
+  //getTrancation(takes the accountId)
 }
