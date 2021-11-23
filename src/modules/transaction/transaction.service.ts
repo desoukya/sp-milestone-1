f1f97a25f7ef76201ebe0193ff98b51fab12e9a6
omaima-9 <satsicko@gmail.com>
2021-11-23 21:02:04 +0200

    Added findAll method  in trasaction service

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
+<<<<<<< Updated upstream
 export class TransactionService {
   // TODO: Define your Transaction Service Logic
 
   //getTrancation(takes the accountId)
 }
+=======
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  /**
+   * Returns all users from mongo database
+   */
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+}
+>>>>>>> Stashed changes
