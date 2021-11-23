8eb7a72a654633883ff5c8bf2b6dab8f24547bf3
Kerelose <keromalak52@outlook.com>
2021-11-23 20:40:33 +0200

    Update transaction.service.ts

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TransactionService {
+  constructor(public id: number, public debit: number, public transactionName: string, 
+    public date: Date, public credit: number, public activebankaccount: string, public totalAmount: number){}
+    
+    getLedgerDate(): string {
+
+      let options: Intl.DateTimeFormatOptions = {
+          day: "numeric", month: "numeric", year: "numeric",
+          hour: "2-digit", minute: "2-digit"
+      };
+
+      return this.date.toLocaleDateString("en-GB", options) + " " + this.date.toLocaleTimeString("en-GB", options);
+   }
+
+
   // TODO: Define your Transaction Service Logic
 }
