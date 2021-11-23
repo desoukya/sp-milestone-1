8bc5d26a744e4f383679f447f161658f039cff00
Tasneem82 <tasneem.moussa82@gmail.com>
2021-11-23 21:01:46 +0200

    added in transaction controller findAll()

--- src/modules/transaction/transactions.controller.ts
+++ src/modules/transaction/transactions.controller.ts
@@ -1,6 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TransactionService } from './transaction.service';
 
 @Controller('transactions')
 export class TransactionController {
   // TODO: Define your Transaction Endpoints
+  constructor(private transactionService: TransactionService) {}
+
+  /**
+   * API endpoint handler returns the authenticated user from JWT payload
+   */    
+  //@UseGuards(AuthGuard('jwt'))
+  @Get()
+  transaction(@Request() req: any): any {
+    return req.transaction;
+  }
+
+  /**
+   * API endpoint handler returns all users from mongo database
+   */
+  //@UseGuards(AuthGuard('jwt'))
+  @Get('list')
+  transactions(): any {
+    return this.transactionService.findAll();
+  }
 }
