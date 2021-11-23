f8e9589d4bf8df7927dbb0cab9e17287e4c3b565
ktayyeb <khaledwaleed180@gmail.com>
2021-11-23 17:34:37 +0200

    Updated MONGO_URL and user.schema
    -Updated MONGO_URL at .env with our cluster.
    
    -Updated the user's schema by adding the properties: [password, username, phone ,GIU Email, GIU Student ID ].
    
    Co-Authored-By: karimSalem25 <82684438+karimSalem25@users.noreply.github.com>
    Co-Authored-By: yusufbadawi <48536215+yusufbadawi@users.noreply.github.com>

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -18,6 +18,24 @@ export class User {
 
   @Prop({ required: true })
   email: string;
+
+  @Prop({ required: true })
+  password:string;
+
+  @Prop({ required: true })
+  userName:string;
+
+  @Prop({ required: true })
+  phone:number;
+
+  @Prop({ required: true })
+  giuEmail:string;
+
+  @Prop({ required: true })
+  giuId:number;
+
+  
+
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
