e04d97c3b76867d3d1509274766f3e000ab750d6
ktayyeb <khaledwaleed180@gmail.com>
2021-11-24 17:02:46 +0200

    Update user.service.ts
    Add createUser method that will add a new user to the users collection.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,15 +3,25 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '@sp/schemas';
 import { Model } from 'mongoose';
 import passport from 'passport';
+import {UserDto} from './dtos/user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+
+  
   /**
    * Returns all users from mongo database
    */
   findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
+
+  createUser(dto: UserDto):Promise<User>{
+     
+    const newUser = new this.userModel(dto);
+    return newUser.save();  
+  }
+
 }
