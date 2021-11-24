595ef4ae18f6179a4d8514ebc213502f39ff07fd
karimSalem25 <karim.salem@student.giu-uni.de>
2021-11-24 17:35:26 +0200

    Add user.dto.ts

---
+++ src/modules/user/dtos/user.dto.ts
@@ -0,0 +1,26 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class UserDto {
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  userName: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  phone: Number;
+
+  @IsEmail()
+  giuEmail: string;
+
+  @IsNotEmpty()
+  giuId: Number;
+
+}
