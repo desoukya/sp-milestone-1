93f3b4cff3b3dec25406d3b697b3deab48281a52
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2021-11-26 21:10:05 +0200

    merge

--- server/src/modules/transaction/transactions.controller.ts
+++ server/src/modules/transaction/transactions.controller.ts
@@ -1,6 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Request, Param } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { transactionDto } from './transaction.dto';
+import { TransactionService } from './transaction.service';
 
 @Controller('transactions')
 export class TransactionController {
-  // TODO: Define your Transaction Endpoints
+  constructor(private transactionService: TransactionService) { }
+
+  @Post('/newTransaction') 
+  async createTransaction(@Body() transaction: transactionDto) {
+      return await this.transactionService.newTransaction(transaction);
+  }
+
+  @Get('/:id/transactionList')
+  async transaction(@Param('id') id: string){
+    console.log("l id aho lli da5el: " + id);
+    return await this.transactionService.findTransaction(id);
+  }
 }
