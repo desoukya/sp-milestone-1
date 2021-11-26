49393c7bcb3015c19fba3edf91fef62e98ee8ec6
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2021-11-26 21:10:10 +0200

    Update transaction.service.ts

--- server/src/modules/transaction/transaction.service.ts
+++ server/src/modules/transaction/transaction.service.ts
@@ -1,6 +1,94 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, Post } from '@nestjs/common';
+import { NestApplicationContext } from '@nestjs/core';
+import { InjectModel } from '@nestjs/mongoose';
+//import { Transaction } from '@sp/schemas';
+import { Model } from 'mongoose';
+import { transactionDto } from './transaction.dto';
+import { Transaction } from '../transaction/transaction.interface';
 
 @Injectable()
-export class TransactionService {
-  // TODO: Define your Transaction Service Logic
+export class TransactionService { 
+  constructor(@InjectModel('Transaction') private transactionModel : Model<Transaction>) {}
+  
+  findAll(): Promise<Transaction[]> {
+    return this.transactionModel.find().exec();
+  }
+
+  async findTransaction(id): Promise<Transaction> {
+    console.log("l id aho lli da5el serfise: " + id);
+    const meow = this.transactionModel.findOne({id : id});
+    console.log((await meow).transactionName);
+    return await this.transactionModel.findOne({id : id});
+
+  }
+
+  async newTransaction(transactionDto: transactionDto) {
+    let transaction = new this.transactionModel(transactionDto);
+    return await transaction.save();
+  }
+
 }
+
+    
+   /*
+    _isPositive(amount: number): boolean{
+      
+      const isPositive = amount > 0;
+      if(!isPositive){
+        console.error('Amount Must be Positive');
+        return false;
+      }
+      return true;
+    }
+
+    _isAllowed(amount: number , account : accountDto): boolean{
+      if(!this._isPositive(amount))
+        return false;
+      const isAllowed = account.totalAmount - amount >= 0;
+      if(!isAllowed){
+        console.error('You have insufficient funds!');
+        return false;
+      }
+      return true;  
+    }
+  
+    getLedgerDate(account : Account): string {
+
+      let options: Intl.DateTimeFormatOptions = {
+          day: "numeric", month: "numeric", year: "numeric",
+          hour: "2-digit", minute: "2-digit"
+      };
+
+      return account.date.toLocaleDateString("en-GB", options) + " " + account.date.toLocaleTimeString("en-GB", options);
+   }
+   
+   getTheDeposite(amount : number , account : Account): boolean {
+     if(this._isPositive(amount)){
+       account.balance += amount;
+       console.info(`Deposite: ${account.active , account.id} new balance is ${account.balance}`);
+       return true;
+     }
+     return false;
+   }
+   withDraw(amount : number , account : accountDto ) : boolean{
+     if(this._isAllowed(amount , account)){
+        account.totalAmount -= amount;
+       console.info(`Withdraw: ${this.accountModel , this.accountModel} new balance is ${this.accountModel}`)
+       return true;
+     }
+     return false;
+   }
+   MakeTransaction(amount: number , account : accountDto) : boolean{ 
+     this.withDraw(amount , account); 
+     account.totalAmount += amount;
+     return true; 
+   }*/
+   /*function Account(name:string , balance : number) {
+
+     this.name = name;
+     this.balance = balance;
+     
+   }*/
+
+
+  // TODO: Define your Transaction Service Logic
