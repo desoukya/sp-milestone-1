73fc08b78a62bda305d7bd9353c89a6321bfde9f
karimSalem25 <karim.salem@student.giu-uni.de>
2021-11-26 22:07:04 +0200

    level 1 code
    Co-Authored-By: Tasneem82 <94862430+Tasneem82@users.noreply.github.com>
    Co-Authored-By: Nourankh20 <82681541+Nourankh20@users.noreply.github.com>

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Dashboard() {
+  const [accounts, viewAccounts] = useState([]);
+  useEffect(() => {
+  console.log("Mounting!");
+  axios
+  .get(`http://localhost:5000/accounts/${userid}`)
+  .then((response) => viewTransactions(response.data));
+  }, []);
+
+
+
   return <div>DASHBOARD</div>;
 }
--- client/pages/transactions.js
+++ client/pages/transactions.js
@@ -1,5 +1,45 @@
 import React from "react";
 
 export default function transactions() {
-  return <div>{/* ADD LOGIC HERE */}</div>;
+  const [transactions, viewTransactions] = useState([]);
+useEffect(() => {
+console.log("Mounting!");
+axios
+.get(`http://localhost:5000/transactions/${accountId}`)
+.then((response) => viewTransactions(response.data));
+}, []);
+  return <div>{/* ADD LOGIC HERE */
+<div class="container">
+  <h2>Basic Table</h2>
+  <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Transaction Name</th>
+        <th>Debit</th>
+        <th>Credit</th>
+        <th>ammount</th>
+        <th>Account ID</th>
+      </tr>
+    </thead>
+    <tbody>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="ID" variant="outlined" value={transaction.Display_Date} 
+      />
+      <TextField id="outlined-basic" label="Name" variant="outlined" value={student.studentName} />
+      <TextField id="outlined-basic" label="Grade" variant="outlined" value={student.grade} />
+      <TextField id="outlined-basic" label="Major" variant="outlined" value={student.section} />
+      <Button variant="contained" color="primary" onClick = {(CreateStudent)}>
+        Create
+      </Button>
+    </form>
+    </tbody>
+  </table>
+</div>
+
+
+
+    
+  }</div>;
 }
