326f69e799023efa47a001cfdce4f4c7c149ad1c
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2021-11-27 18:08:46 +0200

    Update user.js

--- client/adapters/user.js
+++ client/adapters/user.js
@@ -1,9 +1,15 @@
 import apiService from "../services/apiService";
-import { useQuery, useQueryClient, useMutation } from "react-query";
+import {
+  useQuery,
+  useQueryClient,
+  useMutation
+} from "react-query";
 
 export function useFetchUser(userId) {
   return useQuery(["userData", userId], () =>
-    apiService.get(`user/${userId}`).then(({ data }) => data)
+    apiService.get(`user/${userId}`).then(({
+      data
+    }) => data)
   );
 }
 
@@ -12,25 +18,18 @@ export function useMutateLoginUser() {
     (user) => {
       const data = new FormData();
       data = {
-        email : user.email,
-        password : user.password
+        email: user.email,
+        password: user.password
       }
-      //const myUser = window.localStorage.setItem("myUser", user);
+      const myUser = window.localStorage.setItem("myUser", user);
       return apiService.post(`http://localhost:8000/auth/login`, data);
-    },
-    {
+    }, {
       // When mutate is called:
       onSuccess: (responseData) => {
         // Store Token in local storage
-        //console.log("tweety was here : ");
-        //const user = window.localStorage.getItem("myUser");
-        //console.log(user);
-      
-        const myToken= window.localStorage.setItem("jwt", user);
-        
+        const user = window.localStorage.getItem("myUser");
+        const myToken = window.localStorage.setItem("jwt", user);
         //window.localStorage.getItem("jwt");
-        //this.props.history.push("/home")
-
 
       },
       onError: (e) => console.log(e.message),
@@ -46,8 +45,7 @@ export function useMutateRegisterUser() {
       data.append("email", user.email);
       data.append("password", user.password);
       return apiService.post(`user/register`, data);
-    },
-    {
+    }, {
       // When mutate is called:
       onSuccess: (responseData) => {
         // Redirect to login page
@@ -66,15 +64,13 @@ export function useMutateUpdateUser(userId) {
       data.append("email", user.email);
       data.append("password", user.password);
       return apiService.post(`user/${userId}`, data);
-    },
-    {
+    }, {
       // When mutate is called:
       onSuccess: (responseData) => {
         return queryClint.setQueryData(
           ["userData", userId],
           (data) => {
-            return [
-              {
+            return [{
                 email: responseData.data.body.email,
                 password: responseData.data.body.password,
               },
