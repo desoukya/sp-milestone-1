818164c40b52848e28400a51c6fd55b48e61faee
Abdelrahman Hany <34286982+Karitori@users.noreply.github.com>
2021-11-27 17:08:18 +0200

    Update _app.js to fix the default Myapp function

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/Navbar";
+import {
+  QueryClient,
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "react-query";
+
+const QueryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <QueryClientProvider client={QueryClient}>
       <Component {...pageProps} />
-    </div>
+    </QueryClientProvider>
   );
 }
 
