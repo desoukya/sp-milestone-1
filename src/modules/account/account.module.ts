d3bcdb0c39bb53ba9042629cc5d5e0082f1611e7
ktayyeb <khaledwaleed180@gmail.com>
2021-11-28 20:03:06 +0200

    Fix account.module.ts

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Account, AccountSchema } from '@sp/schemas';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import {TransactionModule} from '../transaction/transaction.module';
+
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),TransactionModule],
   exports: [AccountService],
   controllers: [AccountController],
   providers: [AccountService],
