10b9d5dfb97d2786a243728a7810c8287c7fcfe8
amrsamir01 <amr01158148666@gmail.com>
2021-11-30 15:00:03 +0200

    a

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '@sp/schemas';
 import { Model } from 'mongoose';
@@ -13,8 +13,18 @@ export class UserService {
   findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
-  createUser(req):any{
-    req.User = users.find(users => GIU_id === GIU_id); 
+  private sanitizedUser(user: User){
+    return user.validatePassword('password');
+  }
+   createUser(req):any{
+    const username = req;
+    const user = this.userModel.findOne({username});
+    if(user){
+      throw new HttpException('user already exist', HttpStatus.BAD_REQUEST);
+    }
+    const createUser = new this.userModel(req);
+    createUser.save();
+    return this.sanitizedUser(createUser);
     //add the users to database
   }
 
