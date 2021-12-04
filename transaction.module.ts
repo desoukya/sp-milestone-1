ebff46a4e5d1a0406419053cb26fa70b8d6f753b
Shahenda-Magdy <85845088+Shahenda-Magdy@users.noreply.github.com>
2021-12-05 00:48:08 +0200

    Add files via upload

---
+++ transaction.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {  transcations, transcationsSchema } from './transaction.schema';
+import { TransactionController } from './transactions.controller';
+import { transactionService } from './transaction.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: transcations.name, schema: transcationsSchema }])],
+  exports: [transactionService],
+  controllers: [TransactionController],
+  providers: [transactionService],
+})
+export class TransactionModule {}
---
+++ transactions.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { useState } from "react";
+export default function transactions(props) {
+return <li className="transactions">{props.accountid}</li>;}
+
