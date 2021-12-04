5f9c30ce1674c47a27fe16a176511cc0c9324e2c
amrsamir01 <amr01158148666@gmail.com>
2021-12-05 01:08:16 +0200

    a

---
+++ server/src/modules/transaction/transaction.data.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from 'class-validator';
+export class Transactiondata{
+
+    @IsNotEmpty()
+    Display_date: string;
+
+    @IsNotEmpty()
+    debit: number;
+
+
+    credit: number;
+
+    @IsNotEmpty()
+    amount: number;
+
+    @IsNotEmpty()
+    accountid: string;
+
+  }
