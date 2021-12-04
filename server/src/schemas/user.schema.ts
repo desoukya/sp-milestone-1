9dd67418d4cca9edad9373bab92eaf130f5b7cb0
karimSalem25 <karim.salem@student.giu-uni.de>
2021-12-04 21:19:04 +0200

    adding detailed comments

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -12,53 +12,46 @@ export type UserDocument = User & Document;
   For additional info, visit: https://docs.nestjs.com/techniques/mongodb
 */
 
-//@typedef {Object}  nameOfSchema
-@Schema()
 
+/*
+@typedef {Object}  nameOfSchema
+a required property named firstName of type string to get firstname of the user 
+a required property named lastName of type string to get lastname of the user 
+a required property named userId of type Number to get user's Id 
+a required property named email of type string to get email of the user
+a required property named password of type string to get password of the user 
+a required property named phone of type string to get user's phone number
+example for user to registerhe must add 
+firstName : "karim", 
+lastName:"karim" , 
+userId:1001619, 
+email: "karim.salem@student.giu-uni.de" 
+, password:"K2510salem"
+,phone"+201000008993" 
+if user left any of properties empty or added a string instead of number he/she won't be able to register
+*/
+
+@Schema()
 export class User {
-  /*
-  a required property named firstName of type string to get firstname of the user 
-  */
+
   @Prop({ required: true })
   firstName: string;
-  /*
-    a required property named lastName of type string to get lastname of the user 
-    */
+
   @Prop({ required: true })
   lastName: string;
-  /*
-    a required property named userId of type Number to get user's Id 
-    */
+
   @Prop({ required: true })
   userId: Number;
-  /*
-    a required property named email of type string to get email of the user 
-    */
+
   @Prop({ required: true })
   email: string;
-  /*
-    a required property named password of type string to get password of the user 
-    */
+
   @Prop({ required: true })
   password: string;
-  /*
-    a required property named phone of type string to get user's phone number 
-    */
+
   @Prop({ required: true })
   phone: string;
 
-  /* example for user to registerhe must add 
-  firstName : "karim", 
-  lastName:"karim" , 
-  userId:1001619, 
-  email: "karim.salem@student.giu-uni.de" 
-  , password:"K2510salem"
-  ,phone"+201000008993" 
-  */
-  
-  /*
-  if user left any of properties empty or added a string instead of number he/she won't be able to register
-  */
 
 }
 
