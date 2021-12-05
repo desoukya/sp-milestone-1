8e22137f1b4b3a0c2a436c0ec60691139fa03b80
amrsamir01 <amr01158148666@gmail.com>
2021-12-05 21:29:56 +0200

    a

---
+++ client/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Dashboard() { //return <div>DASHBOARD</div>;
+
+  return(
+   <div>
+   <p className="title" > transaction </p>
+
+
+   </div>
+   );
+
+}
